e468a0b23076a1212ad65bd24f17a78723b8006c
Baubek <unreal-kz@users.noreply.github.com>
2017-11-18 22:13:29 -0500

    Update RectangleTest.java

--- RectangleTest.java
+++ RectangleTest.java
@@ -4,17 +4,14 @@ public class RectangleTest {
 public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
 	Rectangle r1=new Rectangle();
-
+	int width = 0;
+	int length = 0;
 	System.out.println("What is the width? ");
-    r1.width=in.nextInt();
-	
-    System.out.println("What is the length? ");
-    r1.length=in.nextInt();
-    r1.area=r1.width*r1.length;
-    
-     		
+    	width=in.nextInt();	
+    	System.out.println("What is the length? ");
+    	length=in.nextInt();	
    
-	System.out.println("Rectangle area is  "+r1.area);
+	System.out.println("Rectangle area is  "+r1.getArea(length,width);
 	
 }
 }
